eff58d5394811ddb5e4b69d9e3d40c84467b9310
hisham tarek <heshamtarek.7222@gmail.com>
2022-07-21 19:44:24 +0200

    ss

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,16 +6,19 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\Translatable\HasTranslations;
 
 class Category extends Model implements HasMedia
 {
-    use HasFactory , InteractsWithMedia;
+    use HasFactory , InteractsWithMedia , HasTranslations;
 
     protected $fillable = [
         'name',
         'description',
         'is_active'
     ];
+    public $translatable = ['name','description'];
+
     protected $casts=[
         'is_active'=>'boolean'
     ];
