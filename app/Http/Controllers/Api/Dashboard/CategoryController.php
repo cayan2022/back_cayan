cb2120ee1b712e09765a221beffe7bd78252d1d7
Mohamed-Nasr-ALi <mn5740153@gmail.com>
2022-07-26 16:00:36 +0200

    fix translation

--- app/Http/Controllers/Api/Dashboard/CategoryController.php
+++ app/Http/Controllers/Api/Dashboard/CategoryController.php
@@ -22,7 +22,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        return CategoryResource::collection(Category::paginate());
+        return CategoryResource::collection(Category::all());
     }
 
     /**
