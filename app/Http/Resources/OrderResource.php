4649ce2b4f81a2a51f2765368c89c8bdeddb302f
Mahmoud Maher Mohamed Nazer <mahmoudmaherfcih30051994@gmail.com>
2023-06-16 17:40:14 +0300

    update order resource

--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -36,7 +36,7 @@ class OrderResource extends JsonResource
     private function FollowUpDate()
     {
         if ($this->status_id == 2 && $this->histories != null) {
-            $follow_up_date = $this->histories->duration;
+            $follow_up_date = $this->histories->first()->duration;
             if ($follow_up_date != null) {
                 $now = Carbon::now();
                 if ($now->greaterThan($follow_up_date)) {
