0aeb4c65dd85e98515555e0592383a0668aaf14a
Mahmoud Maher Mohamed Nazer <mahmoudmaherfcih30051994@gmail.com>
2023-06-16 20:03:11 +0300

    update order controller

--- app/Http/Resources/Reports/ModeratorsReportResource.php
+++ app/Http/Resources/Reports/ModeratorsReportResource.php
@@ -16,14 +16,25 @@ class ModeratorsReportResource extends JsonResource
      */
     public function toArray($request)
     {
+        if ($request->filled('start_date') && $request->filled('end_date')) {
+            $orders = Order::whereBetween('updated_at', [$request->get('start_date'), $request->get('end_date')])
+                ->whereHas('histories', function ($q) {
+                    $q->where('user_id', $this->id);
+                })->get();
+        } else {
+            $orders = Order::whereHas('histories', function ($q) {
+                $q->where('user_id', $this->id);
+            })->get();
+        }
+
         return [
             'id' => $this->id,
             'name' => $this->name,
             'image' => $this->getAvatar(),
             'type' => $this->type,
-            'total_orders' =>(int) $this->orders->count(),
-            'percentage_to_all_orders'=>(float) $this->orders->count() / Order::count() ,
-            'orders_statuses' => (new StatusReportCollection(Status::all()))->additional(['orders'=>$this->orders]),
+            'total_orders' => (int) $orders->count(),
+            'percentage_to_all_orders' => (float) $orders->count() / Order::count(),
+            'orders_statuses' => (new StatusReportCollection(Status::all()))->additional(['orders' => $orders]),
         ];
     }
 }
