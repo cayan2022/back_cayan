bb86b90e55edd6046641c0b33e2a03fd630933f9
Mahmoud Maher Mohamed Nazer <mahmoudmaherfcih30051994@gmail.com>
2023-06-25 00:42:52 +0300

    update status

--- app/Http/Controllers/Api/Dashboard/Reports/StatusesReportController.php
+++ app/Http/Controllers/Api/Dashboard/Reports/StatusesReportController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Api\Dashboard\Reports;
 
+use App\Models\OrderHistory;
 use App\Models\Status;
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Resources\StatusResource;
 
@@ -17,6 +19,41 @@ class StatusesReportController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return StatusResource::collection(Status::filter()->get());
+        if ($request->filled('start_date') && $request->filled('end_date') && $request->filled('employee')) {
+            $user = User::where('name', $request->get('employee'))->first();
+            $order_ids = OrderHistory::where('user_id', $user->id)->groupBy('order_id')->pluck('order_id');
+
+            $statuses = Status::withCount([
+                'orders' => function ($query) use ($request, $order_ids) {
+                    $query->whereBetween('created_at', [$request->get('start_date'), $request->get('end_date')])
+                        ->whereIn('id', $order_ids);
+                },
+            ])->filter()->get();
+
+        } elseif ($request->filled('start_date') && $request->filled('end_date') && !$request->filled('employee')) {
+            $statuses = Status::withCount([
+                'orders' => function ($query) use ($request) {
+                    $query->whereBetween(
+                        'created_at',
+                        [
+                            $request->get('start_date'),
+                            $request->get('end_date'),
+                        ]
+                    );
+                },
+            ])->filter()->get();
+        } elseif (!$request->filled('start_date') && !$request->filled('end_date') && $request->filled('employee')) {
+            $user = User::where('name', $request->get('employee'))->first();
+            $order_ids = OrderHistory::where('user_id', $user->id)->groupBy('order_id')->pluck('order_id');
+            $statuses = Status::withCount([
+                'orders' => function ($query) use ($order_ids) {
+                    $query->whereIn('id', $order_ids);
+                },
+            ])->get();
+        } else {
+            $statuses = Status::filter()->get();
+        }
+
+        return StatusResource::collection($statuses);
     }
 }
