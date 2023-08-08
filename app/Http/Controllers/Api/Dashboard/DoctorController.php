3ec67f1d192a8bbf40a69953b8a0f6df21d39090
Mahmoud Maher Mohamed Nazer <mahmoudmaherfcih30051994@gmail.com>
2023-08-08 22:59:42 +0300

    update doctors arrangement

--- app/Http/Controllers/Api/Dashboard/DoctorController.php
+++ app/Http/Controllers/Api/Dashboard/DoctorController.php
@@ -121,7 +121,7 @@ class DoctorController extends Controller
         if ($doctor_order->id < $doctor->id) {
             foreach (Doctor::where('id', '=>', $doctor_order->id)->where('id', '<', $doctor->id)->get() as $doctor_data) {
                 $doctor_data->order = $doctor_data->order + 1;
-                $doctor_data->save;
+                $doctor_data->save();
             }
         }
 
