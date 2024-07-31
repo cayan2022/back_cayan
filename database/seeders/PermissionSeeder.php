df06b58725025d675c70bd7ae4209a94a0327630
Mohamed Karem <m.karem456@gmail.com>
2024-07-31 14:15:42 +0300

    edits

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -16,6 +16,7 @@ class PermissionSeeder extends Seeder
     public function run()
     {
         // create permissions
+        Permission::truncate();
         $this->createPermissions();
     }
 
