d37e3f18e2406a102037bcb59913587fb1074645
Mohamed Karem <m.karem456@gmail.com>
2024-09-03 15:11:14 +0300

    add clicks of each source in reports

--- app/Http/Resources/Reports/SourcesReportResource.php
+++ app/Http/Resources/Reports/SourcesReportResource.php
@@ -5,6 +5,7 @@ namespace App\Http\Resources\Reports;
 use App\Models\Order;
 use App\Models\Status;
 use App\Models\Source;
+use App\Models\SourceClicks;
 use App\Http\Resources\StatusResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Http\Resources\Json\ResourceCollection;
@@ -21,11 +22,18 @@ class SourcesReportResource extends JsonResource
     {
         $count = Order::count();
 
+        $clicks = [];
+        $sourceClicks = SourceClicks::where('source_id',$this->id)->get();
+        foreach ($sourceClicks as $key => $sourceClick) {
+            $clicks[$sourceClick->clickable_type] = $sourceClick->clicks;
+        }
+
         return [
             'id' => $this->id,
             'name' => $this->name,
             'short_description' => $this->short_description,
             'total_orders' =>(int) $this->orders->count(),
+            'clicks' => $clicks,
             'percentage_to_all_orders' => $count > 0 ? ((float)$this->orders->count() / Order::count()) : 0,
             'orders_statuses' => (new StatusReportCollection(Status::all()))->additional(['orders'=>$this->orders]),
         ];
