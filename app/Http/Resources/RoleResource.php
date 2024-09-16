704796d7b63835b0f531d2ff361c83030aca48f8
Mohamed Karem <m.karem456@gmail.com>
2024-09-17 02:24:14 +0300

    add campaigns

--- app/Http/Resources/RoleResource.php
+++ app/Http/Resources/RoleResource.php
@@ -9,15 +9,15 @@ class RoleResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
     {
         return [
-            'id'=>$this->id,
-            'name'=>$this->name,
-            'permissions'=> PermissionResource::collection($this->permissions)
+            'id' => $this->id,
+            'name' => __('Roles.' . $this->name),
+            'permissions' => PermissionResource::collection($this->permissions)
         ];
     }
 }
