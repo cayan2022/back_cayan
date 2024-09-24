c3bcd6e60cf5f1c5c4da5b17a7e66defac359476
Mohamed Karem <m.karem456@gmail.com>
2024-09-24 14:22:34 +0300

    edits

--- database/migrations/2022_06_20_223648_create_orders_table.php
+++ database/migrations/2022_06_20_223648_create_orders_table.php
@@ -24,6 +24,7 @@ class CreateOrdersTable extends Migration
             $table->foreignId('status_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
             $table->foreignId('branch_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
 
+            $table->string('type')->nullable();
             $table->softDeletes();
             $table->timestamps();
         });
