2c3b67671c24be7e3e7b139ea40b550457a79beb
Mohamed Karem <m.karem456@gmail.com>
2024-10-02 18:14:15 +0300

    create user tenant

--- app/Http/Resources/SaasOrderResource.php
+++ app/Http/Resources/SaasOrderResource.php
@@ -12,14 +12,15 @@ class SaasOrderResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'category' => $this->category->name,
-            'status' => new StatusResource($this->status),
             'user_name' => $this->user->name,
             'user_phone' => $this->user->phone,
             'user_avatar' => $this->user->getAvatar(),
-            'company_name' => $this->user->company_name,
-            'spec' => $this->user->company_spec,
-            'tenant' => $this->user->tenant_id
+            'email' => $this->user->email,
+            'company_name' => $this->user?->tenant?->company_name,
+            'company_spec' => $this->user?->tenant?->company_spec,
+            'domain' => $this->user?->tenant?->domain,
+            'is_paid' => (boolean) $this->user?->tenant->is_paid,
+            'expired_at' => $this->user?->tenant?->expired_at
         ];
     }
 }
