859a0c4a847bac0ef27ba077baf94d48f2cbd047
Mohamed Karem <m.karem456@gmail.com>
2024-10-06 15:50:56 +0300

    fixes

--- routes/api.php
+++ routes/api.php
@@ -59,8 +59,24 @@ Route:: as('site.')
         Route::post('order', OrderController::class)->name('order.store');
         Route::get('portfolio-categories', PortfolioCategoryController::class)->name('portfolio-categories');
         Route::get('portfolios', PortfolioController::class)->name('portfolios');
-        Route::get('seo_pages', [SeoPageController::class,'index'])->name('seo-pages');
+        Route::get('seo_pages', [SeoPageController::class, 'index'])->name('seo-pages');
 
         // source clicks
-        Route::post('add_click', [SourceClickController::class,'addClick'])->name('add-click');
+        Route::post('add_click', [SourceClickController::class, 'addClick'])->name('add-click');
     });
+
+
+Route::post('getSessionIdMyFatoorah', function () {
+
+    $token = 'Bearer rLtt6JWvbUHDDhsZnfpAhpYk4dxYDQkbcPTyGaKp2TYqQgG7FGZ5Th_WD53Oq8Ebz6A53njUoo1w3pjU1D4vs_ZMqFiz_j0urb_BH9Oq9VZoKFoJEDAbRZepGcQanImyYrry7Kt6MnMdgfG5jn4HngWoRdKduNNyP4kzcp3mRv7x00ahkm9LAK7ZRieg7k1PDAnBIOG3EyVSJ5kK4WLMvYr7sCwHbHcu4A5WwelxYK0GMJy37bNAarSJDFQsJ2ZvJjvMDmfWwDVFEVe_5tOomfVNt6bOg9mexbGjMrnHBnKnZR1vQbBtQieDlQepzTZMuQrSuKn-t5XZM7V6fCW7oP-uXGX-sMOajeX65JOf6XVpk29DP6ro8WTAflCDANC193yof8-f5_EYY-3hXhJj7RBXmizDpneEQDSaSz5sFk0sV5qPcARJ9zGG73vuGFyenjPPmtDtXtpx35A-BVcOSBYVIWe9kndG3nclfefjKEuZ3m4jL9Gg1h2JBvmXSMYiZtp9MR5I6pvbvylU_PP5xJFSjVTIz7IQSjcVGO41npnwIxRXNRxFOdIUHn0tjQ-7LwvEcTXyPsHXcMD8WtgBh-wxR8aKX7WPSsT1O8d8reb2aR7K3rkV3K82K_0OgawImEpwSvp9MNKynEAJQS6ZHe_J_l77652xwPNxMRTMASk1ZsJL';
+    $response = Http::withToken($token)
+        ->post('https://apitest.myfatoorah.com/v2/InitiateSession', [
+            'CustomerIdentifier' => request()->customer_id,
+        ]);
+
+    if ($response->successful()) {
+        // Handle the response
+        return $response->json();
+    }
+    abort($response->status(), 'Error during request.');
+});
