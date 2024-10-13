4e1e80484f105d6e71fb2b144758016a11387f40
Mohamed Karem <m.karem456@gmail.com>
2024-10-13 12:53:39 +0300

    edits

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -35,33 +35,5 @@ class AppServiceProvider extends ServiceProvider
                 ? $rule->mixedCase()->symbols()->numbers()
                 : $rule;
         });
-
-        // Ensure that the user is authenticated before performing the check
-        if (Auth::guard('sanctum')->check()) {
-            // Fetch the date you want to check from the database
-            $user = Auth::guard('sanctum')->user(); // Assuming the date is linked to the authenticated user
-
-            $orders = Order::filter()->whereHas('histories', function ($q) use ($user) {
-                $q->where('user_id', $user->id);
-            })->whereStatusId(2)->get();
-
-            foreach ($orders as $order) {
-                if (Carbon::now()->diffInDays(Carbon::parse($order->histories->last()->duration), false) == 1) {
-                    // Perform your action, e.g., sending a reminder email
-                    $details = [
-                        'email' => $user->email,
-                        'title' => ' تذكير متابعة الطلب' . $order->id,
-                        'body' => $order->id . ' ورقم الطلب  ' . $order->user->phone .' - '.$order->user->name . 'تم التأخير على متابعة العميل ',
-                    ];
-
-                    // Dispatch the email (you can also use a job here)
-//                    if ($order->histories->last()->get_reminder == 0) {
-//                        \Mail::to($details['email'])->send(new ReminderEmail($details));
-//                        $order->histories->last()->update(['get_reminder' => 1]);
-//                    }
-                }
-            }
-
-        }
     }
 }
