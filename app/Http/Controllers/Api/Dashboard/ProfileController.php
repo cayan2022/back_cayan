f252c9774d9d02165502ebfa751e7a31de8ce2bc
Mohamed Karem <m.karem456@gmail.com>
2024-10-22 14:27:06 +0300

    edits

--- app/Http/Controllers/Api/Dashboard/ProfileController.php
+++ app/Http/Controllers/Api/Dashboard/ProfileController.php
@@ -20,29 +20,30 @@ class ProfileController extends Controller
      */
     public function index()
     {
-        return UserResource::collection(User::where('type',User::MODERATOR)->filter()->latest()->paginate());
+        return UserResource::collection(User::where('type', User::MODERATOR)->filter()->latest()->paginate());
     }
 
     /**
-     * @param  StoreProfileRequest  $request
+     * @param StoreProfileRequest $request
      * @return mixed
      */
     public function store(StoreProfileRequest $request)
     {
-        $user = User::create($request->validated()+['type'=>User::MODERATOR]);
-
+        $data = collect($request->validated())->except(['role_id'])->toArray();
+        $user = User::create($data + ['type' => User::MODERATOR]);
         // assign role its has permissions to user
         $role = Role::findOrFail($request->role_id);
 
         $user->assignRole($role);
 
-        if($request->hasFile('image') && $request->file('image')->isValid()){
+        if ($request->hasFile('image') && $request->file('image')->isValid()) {
             $user->addMediaFromRequest('image')
-                ->sanitizingFileName(fn($fileName)=>updateFileName($fileName))
+                ->sanitizingFileName(fn($fileName) => updateFileName($fileName))
                 ->toMediaCollection(User::MEDIA_COLLECTION_NAME);
         }
         return $user->getResource()->additional(['token' => $user->createTokenForDevice($request->header('user-agent'))]);
     }
+
     /**
      * Display the user resource.
      *
@@ -58,9 +59,10 @@ class ProfileController extends Controller
      * @param UpdateProfileRequest $request
      * @return \Illuminate\Http\Resources\Json\JsonResource
      */
-    public function update(UpdateProfileRequest $request,User $user)
+    public function update(UpdateProfileRequest $request, User $user)
     {
-        $user->update($request->validated());
+        $data = collect($request->validated())->except(['role_id'])->toArray();
+        $user->update($data);
 
         // assign role its has permissions to user
         $user->roles()->detach();
@@ -68,17 +70,18 @@ class ProfileController extends Controller
 
         $user->assignRole($role);
 
-        if($request->hasFile('image') && $request->file('image')->isValid()){
+        if ($request->hasFile('image') && $request->file('image')->isValid()) {
             $user->clearMediaCollection(User::MEDIA_COLLECTION_NAME);
             $user->addMediaFromRequest('image')
-                ->sanitizingFileName(fn($fileName)=>updateFileName($fileName))
+                ->sanitizingFileName(fn($fileName) => updateFileName($fileName))
                 ->toMediaCollection(User::MEDIA_COLLECTION_NAME);
         }
 
         return $user->getResource();
     }
+
     /**
-     * @param  User  $user
+     * @param User $user
      * @return \Illuminate\Http\JsonResponse
      */
     public function logout(User $user)
@@ -89,22 +92,23 @@ class ProfileController extends Controller
     }
 
     /**
-     * @param  User  $user
+     * @param User $user
      * @return \Illuminate\Http\JsonResponse
      */
     public function block(User $user)
     {
         $user->block();
         $user->tokens()->delete();
-        return response()->json(['message'=>__('auth.success_operation')]);
+        return response()->json(['message' => __('auth.success_operation')]);
     }
+
     /**
-     * @param  User  $user
+     * @param User $user
      * @return \Illuminate\Http\JsonResponse
      */
     public function active(User $user)
     {
         $user->active();
-        return response()->json(['message'=>__('auth.success_operation')]);
+        return response()->json(['message' => __('auth.success_operation')]);
     }
 }
