1eb57425faac4203ed50dc834cd6837ad3c6f842
Mohamed Karem <m.karem456@gmail.com>
2024-11-08 03:34:52 +0200

    packages

--- app/Models/Package.php
+++ app/Models/Package.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Http\Resources\PackageResource;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -18,6 +19,10 @@ class Package extends Model
         'models' => 'array',
     ];
 
+    public function getResource(): PackageResource
+    {
+        return new PackageResource($this->fresh());
+    }
     public function models()
     {
         return AppModel::whereIn('id', $this->models)->select('id', 'name')->get();
