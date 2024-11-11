138c7fc394435392449bd6ce5434505cd9889ceb
Mohamed Karem <m.karem456@gmail.com>
2024-11-12 00:46:11 +0200

    edits

--- database/migrations/2024_11_10_113557_add_package_to_user_tenant_table.php
+++ database/migrations/2024_11_10_113557_add_package_to_user_tenant_table.php
@@ -12,7 +12,6 @@ class AddPackageToUserTenantTable extends Migration
             $table->foreignId('package_id')->nullable()->constrained()->cascadeOnUpdate()->cascadeOnDelete();
             $table->json('active_models')->nullable();
             $table->json('templates')->nullable();
-            $table->foreignId('template_id')->nullable()->constrained()->cascadeOnUpdate()->cascadeOnDelete();
             $table->string('active_template')->nullable();
         });
     }
@@ -20,7 +19,7 @@ class AddPackageToUserTenantTable extends Migration
     public function down()
     {
         Schema::table('user_tenants', function (Blueprint $table) {
-            $table->dropColumn(['package_id', 'active_models', 'templates','template_id', 'active_template']);
+            $table->dropColumn(['package_id', 'active_models', 'templates', 'active_template']);
         });
     }
 }
