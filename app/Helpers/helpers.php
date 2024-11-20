5d9666ef442ed802ce600ba299eb0dad863c962e
Mohamed Karem <m.karem456@gmail.com>
2024-11-20 12:49:56 +0200

    edits

--- app/Helpers/helpers.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (! function_exists('updateFileName')) {
-    function updateFileName($fileName): string
-    {
-        return strtolower(str_replace(['#', '/', '\\', ' '], '-', time().trim($fileName)));
-    }
-}
--- resources/lang/en/supported.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'images' => [
-        'not_supported' => 'The file does not meet the specifications'
-    ],
-    'notfound'=>'Not Found!',
-    'server_error' => 'If you have a problem registering please call us'
-];
